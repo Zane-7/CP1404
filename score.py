09c91952a24deeef3c444bcf73d27f763e541759
zhangliang0711 <1431751407@qq.com>
2022-11-19 17:02:28 +0800

    1404-prac 2

--- score.py
+++ score.py
@@ -1,6 +1,11 @@
+import random
+
+
 def main():
     score = float(input("Enter score: "))
-    print(determine_status(score))
+    print(determine_status(score,))
+    random_score = determine_randon()
+    print(f"{random_score}\n{determine_status(random_score)}")
 
 
 def determine_status(score):
@@ -13,5 +18,9 @@ def determine_status(score):
     else:
         return "Bad"
 
+
+def determine_randon():
+    randon_score = random.randint(0, 100)
+    return randon_score
 
 main()
